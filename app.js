f4077d3c8bccd3c2b4cd10360c71263a872bb374
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-07 12:30:00 +0530

    Final notes app with localStorage, edit, delete and premium UI

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-let notes = [];
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+function saveNotes(){
+    localStorage.setItem("notes", JSON.stringify(notes));
+}
 
 function addNote(){
     const input = document.getElementById("noteInput");
@@ -9,6 +13,7 @@ function addNote(){
     notes.push(text);
     input.value = "";
 
+    saveNotes();
     renderNotes();
 }
 
@@ -16,11 +21,45 @@ function renderNotes(){
     const list = document.getElementById("notesList");
     list.innerHTML = "";
 
-    notes.forEach(note => {
+    notes.forEach((note, index) => {
+
         const div = document.createElement("div");
         div.className = "note";
-        div.innerText = note;
+
+        const span = document.createElement("span");
+        span.innerText = note;
+
+        const editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        editBtn.onclick = () => editNote(index);
+
+        const delBtn = document.createElement("button");
+        delBtn.innerText = "Delete";
+        delBtn.onclick = () => deleteNote(index);
+
+        div.appendChild(span);
+        div.appendChild(editBtn);
+        div.appendChild(delBtn);
 
         list.appendChild(div);
     });
 }
+
+function editNote(index){
+    const updated = prompt("Edit note:", notes[index]);
+
+    if(updated !== null && updated.trim() !== ""){
+        notes[index] = updated.trim();
+        saveNotes();
+        renderNotes();
+    }
+}
+
+function deleteNote(index){
+    notes.splice(index, 1);
+    saveNotes();
+    renderNotes();
+}
+
+// Load stored notes
+renderNotes();
